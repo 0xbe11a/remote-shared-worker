ee2648fdc3e804d79f8acf3f7e6de2f1e6af8e0c
Bella Chan. <chanyanan.sethpitak2@lseg.com>
2024-08-28 18:53:14 +0700

    dev friendly version

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ if (typeof window !== "undefined") {
     window.SharedWorker = ((BaseSharedWorker) => 
       class PatchedSharedWorker extends BaseSharedWorker {
         constructor(scriptURL, options) {
-          const url = String(scriptURL);
+          const urlVariable = String(scriptURL);
+          const twistedImport = 
+          'const originalImportScripts = importScripts;\n' +
+          'importScripts = function(...urls) {\n' +
+          '    const resolvedUrls = urls.map(url => new URL(url, "' + urlVariable + '").toString());\n' +
+          '    originalImportScripts(...resolvedUrls);\n' +
+          '};\n' +
+          'importScripts("' + urlVariable + '");';
   
           super(
             url.includes("://") && !url.startsWith(location.origin)
               ? URL.createObjectURL(
                   new Blob([
-                    `importScripts=((i)=>(...a)=>i(...a.map((u)=>''+new URL(u,"${url}"))))(importScripts);importScripts("${url}")`
+                    twistedImport
                   ], {
                     type: "text/javascript"
                   })
